aa7591e5cf01da96491ecd1160466013d4e8c47e
matsth <93190019+matsth@users.noreply.github.com>
2022-10-25 08:25:59 -0700

    Smacktown

--- Hero with a stick!/Smacktown.java
+++ Hero with a stick!/Smacktown.java
@@ -1,20 +1,20 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
+ * Write a description of class Smacktown here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class MyWorld extends World
+public class Smacktown extends World
 {
     public int maxVillain;
     public int currentVillain = 0;
     /**
-     * Constructor for objects of class MyWorld.
+     * Constructor for objects of class Smacktown.
      * 
      */
-    public MyWorld()
+    public Smacktown()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(800, 500, 1); 
