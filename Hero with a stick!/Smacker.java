edb000ef714aaec34b511560517302a371fddc26
matsth <93190019+matsth@users.noreply.github.com>
2022-11-01 07:26:56 -0700

    Better now

--- Hero with a stick!/Smacker.java
+++ Hero with a stick!/Smacker.java
@@ -19,6 +19,7 @@ public class Smacker extends Actor
         image.scale(image.getWidth()/10, image.getHeight()/10);
         setImage(image);
         
+        lifes = 3;
         movementspeed = 4;
         hasStick = false;
         lastMove[0] = true;
--- Hero with a stick!/Smacktown.java
+++ Hero with a stick!/Smacktown.java
@@ -10,14 +10,22 @@ public class Smacktown extends World
 {
     public int maxVillain;
     public int currentVillain = 0;
+    
+    public int maxPowers;
+    public int currentPowers = 0;
+    
+    public int timer = 300;
+    private int ticker = 0;
+    
     /**
      * Constructor for objects of class Smacktown.
      * 
      */
     public Smacktown()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(800, 500, 1); 
+        // Create a new world with 800x500 cells with a cell size of 1x1 pixels.
+        super(800, 500, 1);
+        
         prepare();
         setPaintOrder(Smacker.class, Stick.class, Powerups.class, Villain.class);
     }
@@ -28,5 +36,40 @@ public class Smacktown extends World
     public void prepare()
     {
         addObject(new Smacker(), 400, 250);
+        showTime();
+        showlife();
+    }
+    
+    public void act()
+    {
+        timer();
+    }
+    
+    public void timer()
+    {
+        ticker++;
+        
+        if(ticker >= 60)
+        {
+            timer--;
+            ticker = 0;
+            
+            showTime();
+            
+            if(timer <= 0)
+            {
+                this.stopped();
+            }
+        }
+    }
+    
+    private void showTime(){
+        showText("Time: " + timer, 690, 25);
+    }
+    
+    public void showlife()
+    {
+        Smacker smacker = this.getObjects(Smacker.class).get(0);
+        showText("Lifes: " + smacker.lifes, 75, 25);
     }
 }
