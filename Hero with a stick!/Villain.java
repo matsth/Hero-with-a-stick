999058b762215660d385811412665d5dfe433178
aha0714 <abrham.habtom@edu.tbz.ch>
2022-11-01 15:09:51 +0100

    Villain_15.09

--- Hero with a stick!/Villain.java
+++ Hero with a stick!/Villain.java
@@ -8,12 +8,59 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Villain extends Actor
 {
+    public int Movementspeed;
+    
     /**
      * Act - do whatever the Villain wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
+    
+       public Villain()
+    {
+        GreenfootImage image = new GreenfootImage("Villain1.png");
+        image.scale(image.getWidth()/10, image.getHeight()/10);
+        setImage(image);
+        Movementspeed = 4;
+        
+    }
+    public void VillainMove()
+    {
+        int X = 0;
+        int Y = 0;
+        Smacker smacker = getWorld().getObjects(Smacker.class).get(0);
+        if(smacker.getX() > this.getX())
+        {
+            X+=Movementspeed;
+            
+        }
+        else if(smacker.getX() < this.getX())
+        {
+            X-=Movementspeed;
+        
+        }
+        if(smacker.getY() > this.getY())
+        {
+            Y+=Movementspeed;
+            
+        }
+        else if(smacker.getY() < this.getY())
+        {
+            Y-=Movementspeed;
+        
+        }
+        
+        this.setLocation(this.getX() + X, this.getY() + Y);
+        
+    }  
     public void act()
     {
-        // Add your action code here.
+        VillainMove();
     }
+    
+    
+    
 }
+
+
+
